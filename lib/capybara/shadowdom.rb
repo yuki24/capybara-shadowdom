95b135c9cd94c036c43bc939ef50e8a6df07d005
Yuki Nishijima <yuki24@hey.com>
2021-10-11 20:31:10 -0400

    First commit

---
+++ lib/capybara/shadowdom.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "shadowdom/version"
+
+module Capybara
+  module Shadowdom
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
