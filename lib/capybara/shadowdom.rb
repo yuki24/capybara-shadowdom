05d1fafd308bee1198dc232f651864f28996bc68
Yuki Nishijima <yuki24@hey.com>
2021-10-24 22:23:28 -0400

    Update document for shadow_root

--- lib/capybara/shadowdom.rb
+++ lib/capybara/shadowdom.rb
@@ -5,15 +5,28 @@ require_relative "shadowdom/version"
 
 module Capybara
   module Shadowdom
-    # Adds a way to retrieve the shadow root object of an element.
+    # Adds a way to retrieve the shadow root object of an element. For example, given the HTML below:
     #
-    # @example
+    #   <awesome-element>
+    #     <!-- #shadow-root start -->
+    #     <span>Hello shadow world!</span>
     #
-    #   within all("sl-input")[0].shadow_root do
-    #     puts text
-    #     assert_text "Email"
-    #     fill_in "user[email]", with: "ynishijima@pivotal.io"
-    #   end
+    #     <input type="text" id="user_name">
+    #     <!-- #shadow-root end -->
+    #   </awesome-element>
+    #
+    # Then You will be able to do:
+    #
+    # within custom_element.shadow_root do
+    #   # Displays "Hello shadow world!":
+    #   puts page.text
+    #
+    #   # Asserts the text within the element:
+    #   assert_text "Hello shadow world!"
+    #
+    #   # Input elements within the shadow DOM will also be accessible:
+    #   fill_in "#user_name", with: "awesome@example.org"
+    # end
     #
     def shadow_root
       root_node = evaluate_script("this.shadowRoot")
