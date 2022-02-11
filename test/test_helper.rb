1dcfa1181126190727f14a94b73cd551ac8e839b
Yuki Nishijima <yuki24@hey.com>
2022-02-11 19:11:58 +0900

    Set up CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -26,11 +26,62 @@ begin
 rescue LoadError
 end
 
+if ENV['BROWSERSTACK_URL']
+  browserstack_url = URI(ENV['BROWSERSTACK_URL'])
+  browserstack_url.user = ENV['BROWSERSTACK_USERNAME'] if ENV['BROWSERSTACK_USERNAME']
+  browserstack_url.password = ENV['BROWSERSTACK_ACCESS_KEY'] if ENV['BROWSERSTACK_ACCESS_KEY']
+
+  os, os_version, browser, browser_version = ENV.fetch('TARGET_BROWSER', 'Windows, 10, Edge, latest').split(", ")
+
+  caps = Selenium::WebDriver::Remote::Capabilities.new(
+    name: "Capybara Shadow DOM",
+    server: browserstack_url.host,
+    user: browserstack_url.user,
+    key: browserstack_url.password,
+    os: os,
+    os_version: os_version,
+    browser: browser,
+    browser_version: browser_version,
+    "browserstack.console": "errors",
+    "browserstack.debug": true,
+    "browserstack.local": true,
+    "browserstack.networkLogs": true,
+  )
+
+  # Safari has some limitations due to their security models so we have to stick with localhost:3000.
+  if browser.downcase == 'safari'
+    Capybara.app_host = "http://localhost"
+    Capybara.server_port = 3000
+  end
+
+  module BrowserstackPatch
+    def reset!
+      @browser&.navigate.to('about:blank')
+    end
+  end
+
+  Capybara::Selenium::Driver.prepend BrowserstackPatch
+
+  Capybara.register_driver :browserstack do |app|
+    Capybara::Selenium::Driver.new(app, browser: :remote, url: browserstack_url.to_s, desired_capabilities: caps)
+  end
+end
+
 Capybara.register_driver :safari do |app|
   Capybara::Selenium::Driver.new(app, browser: :safari)
 end
 
-driver = ENV['JS_DRIVER']&.to_sym || :selenium_chrome
+driver = ENV['JS_DRIVER']&.to_sym || case ENV['BUNDLE_GEMFILE']
+                                     when /selenium_webdriver/
+                                       ENV['JS_DRIVER'] || :selenium_chrome_headless
+                                     when /cuprite/
+                                       :cuprite
+                                     when /apparition/
+                                       :apparition
+                                     else
+                                       :selenium_chrome_headless
+                                     end
+
 Capybara.server = :webrick
 Capybara.default_driver = driver
 Capybara.javascript_driver = driver
